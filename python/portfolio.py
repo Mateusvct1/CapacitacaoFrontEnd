0ae004eb65845531683cf13b97ad4d5f4e304f1b
MidhiaQ <queirozmidhia9@gmail.com>
2024-03-18 18:33:50 -0300

    cores

---
+++ python/portfolio.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+portfolio = {
+    "Nome": "Mateus Victor",
+    "Descrição": "Texto...",
+    "Projetos/Cursos": ["Projetos"],
+    "Habilidades": ["..."],
+    "Contato": "..."
+}
+
+@app.route('/portfolio', methods=['GET'])
+def get_portfolio():
+    return jsonify(portfolio)
+
+if __name__ == '__main__':
+    app.run(debug=True)
